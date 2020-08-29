9d12c8f24b41414f98afbc3e62a562f40d1107f7
Anil Rayamajhi <anilrayamajhi@Anils-iMac.local>
2020-08-28 18:59:56 -0700

    Assignment 4.4

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// (function () {
+//   if ($(window).width() > 992) {
+//     $(window).scroll(function () {
+//       if ($(this).scrollTop() > 40) {
+//         $("#navbar_top").addClass("fixed-top");
+//         // add padding top to show content behind navbar
+//         $("body").css("padding-top", $(".navbar").outerHeight() + "px");
+//       } else {
+//         $("#navbar_top").removeClass("fixed-top");
+//         // remove padding top from body
+//         $("body").css("padding-top", "0");
+//       }
+//     });
+//   }
+
+//   $("#navbar_top").css("backgroundColor", "red");
+// })();
